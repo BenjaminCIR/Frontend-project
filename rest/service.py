d3148d960ea52a163e2b5d3aa73a0c087d95d8d7
Bwift <benjamin.hubert@student.junia.com>
2026-02-12 14:35:45 +0100

    feat: added pre-commit and ci

--- rest/service.py
+++ rest/service.py
@@ -1,6 +1,7 @@
 import httpx
 from pydantic import BaseModel, Field
 
+
 class Chat(BaseModel):
     """Base class for what a generic POST request to an LLM should contain.
 
@@ -12,6 +13,7 @@ class Chat(BaseModel):
     model: str
     temperature: float | None = Field(ge=0.0, le=1.0, default=0.7)
     messages: list[dict[str, str]]
+
 
 class LLMClient:
     """The client used to communicate with the backend LLM."""
@@ -25,28 +27,9 @@ class LLMClient:
 
     def _generate_request(self, chat: Chat) -> tuple[dict, dict, str]:
         """Generates the 3 parts necessary for the request via the HTTPX library.
-        
-        This function generates the header, body, and url for a POST request via HTTPX.
-        
-        Args:
-            chat (Chat): A Chat class.
-        Returns:
-            tuple[dict, dict, str]: The header, body, and url.
-        """
-        pass
 
-    def post(
-        self,
-        chat: Chat,
-    ):
-        """POST request."""
-        pass
-
-    def _generate_request(self, chat: Chat) -> tuple[dict, dict, str]:
-        """Generates the 3 parts necessary for the request via the HTTPX library.
-        
         This function generates the header, body, and url for a POST request via HTTPX.
-        
+
         Args:
             chat (Chat): A Chat class.
         Returns:
@@ -67,30 +50,33 @@ class LLMClient:
 
         route = f"http://{self.root_url}/api/chat"
 
-        return headers, body, route # type: ignore
-    
+        return headers, body, route  # type: ignore
+
     def post(
-            self,
-            chat: Chat,
-        ):
-            """POST request."""
-            headers, body, route = self._generate_request(chat=chat)
+        self,
+        chat: Chat,
+    ):
+        """POST request."""
+        headers, body, route = self._generate_request(chat=chat)
 
-            try:
-                response = self.client.post(
-                    url=route,
-                    headers=headers,
-                    json=body,
-                    timeout=180.0,
-                )
-                response.raise_for_status()
-            except httpx.RequestError as exc:
-                print(f"An error occurred while requesting {exc.request.url!r}.")
-                raise
-            except httpx.HTTPStatusError as exc:
-                print(f"Error response {exc.response.status_code} while requesting {exc.request.url!r}.")
-                raise
+        try:
+            response = self.client.post(
+                url=route,
+                headers=headers,
+                json=body,
+                timeout=180.0,
+            )
+            response.raise_for_status()
+        except httpx.RequestError as exc:
+            print(f"An error occurred while requesting {exc.request.url!r}.")
+            raise
+        except httpx.HTTPStatusError as exc:
+            print(
+                f"Error response {exc.response.status_code} while requesting {exc.request.url!r}."
+            )
+            raise
 
-            return response
-    
+        return response
+
+
 client = LLMClient(root_url="localhost:11434")
